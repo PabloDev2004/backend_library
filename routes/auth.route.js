0b50113b1d20bbec08d24494ba50f9f123ef8925
Pablo Gutierrez Torres <alex.ander0413tor@gmail.com>
2025-11-02 17:28:33 -0400

    nada

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);
-router.post('/guest', authController.guestLogin);
 
 module.exports = router;
